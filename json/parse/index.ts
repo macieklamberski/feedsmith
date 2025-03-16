ddedbcde94f99b75ed795ed989876533cb40750d
Maciej Lamberski <maciej@lamberski.com>
2025-03-16 12:59:38 +0100

    Update naming in the JSON Feed parse() function

--- json/parse/index.ts
+++ json/parse/index.ts
@@ -7,23 +7,22 @@ export type Options = {
   level: ParseLevel
 }
 
-export type Parse = (json: unknown, options?: Options) => ParsedFeed | undefined
+export type Parse = (value: unknown, options?: Options) => ParsedFeed | undefined
 
-export const parse: Parse = (json, options) => {
+export const parse: Parse = (value, options) => {
   const level: ParseLevel = options?.level || 'coerce'
 
-  // TODO: Implement:
-  // - Ability to define and parse specified additional attributes in feed, head, item.
-  //   extraFeedAttributes?: Array<string>
-  //   extraHeadAttributes?: Array<string>
-  //   extraItemAttributes?: Array<string>
+  // TODO:
+  // - Ability to define and parse specified additional fields in feed and item.
+  //   extraFeedFields?: Array<string>
+  //   extraItemFields?: Array<string>
   // - In strict mode, author should also be converted to authors.
 
   if (level === 'strict') {
-    return parsedFeed.parse(json)
+    return parsedFeed.parse(value)
   }
 
-  const feed = parseFeed(json, level)
+  const feed = parseFeed(value, level)
 
   return feed ? parsedFeed.parse(feed) : undefined
 }
