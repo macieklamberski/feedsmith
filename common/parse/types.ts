a782a6c79a1a5006093cebd7b3b8392dff9794e2
Maciej Lamberski <maciej@lamberski.com>
2025-03-16 13:09:44 +0100

    Create Unreliable type for use with output of fast-xml-parser

--- common/parse/types.ts
+++ common/parse/types.ts
@@ -1,5 +1,15 @@
+import type { ParsedFeed as ParsedJsonFeed } from '../../json/parse/types'
+import type { ParsedFeed as ParsedRssFeed } from '../../rss/parse/types'
+
 export type ParseLevel = 'strict' | 'skip' | 'coerce'
 
 export type NonStrictParseLevel = 'skip' | 'coerce'
 
-export type ParseFunction<T> = (value: unknown, level: NonStrictParseLevel) => T | undefined
+export type Unreliable = { [key: string]: Unreliable } | undefined
+
+export type ParseFunction<R> = (value: unknown, level: NonStrictParseLevel) => R | undefined
+
+export type ParsedFeed = {
+  type: 'json' | 'rss' | 'atom' | 'rdf'
+  feed: ParsedJsonFeed | ParsedRssFeed
+}
