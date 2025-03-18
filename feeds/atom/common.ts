55794c2c236e9783190797fc7a0c0cd8417b0acf
Maciej Lamberski <maciej@lamberski.com>
2025-03-18 21:08:21 +0100

    Make more specific stopNodes setting

--- feeds/atom/common.ts
+++ feeds/atom/common.ts
@@ -2,7 +2,7 @@ import { XMLParser } from 'fast-xml-parser'
 
 export const arrayTags = ['link', 'author', 'contributor', 'category']
 
-export const stopNodes = ['*.content']
+export const stopNodes = ['feed.entry.content']
 
 export const parser = new XMLParser({
   alwaysCreateTextNode: true,
