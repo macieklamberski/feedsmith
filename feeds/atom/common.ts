7b46315904bc60eed4b5148a49c57bb8f314ea51
Maciej Lamberski <maciej@lamberski.com>
2025-03-18 21:30:56 +0100

    Add comprehensive list of nodes to stop parsing further

--- feeds/atom/common.ts
+++ feeds/atom/common.ts
@@ -2,7 +2,67 @@ import { XMLParser } from 'fast-xml-parser'
 
 export const arrayTags = ['link', 'author', 'contributor', 'category']
 
-export const stopNodes = ['feed.entry.content']
+export const stopNodes = [
+  'feed.author.name',
+  'feed.author.uri',
+  'feed.author.url', // Atom 0.3.
+  'feed.author.email',
+  'feed.category',
+  'feed.contributor.name',
+  'feed.contributor.uri',
+  'feed.contributor.url', // Atom 0.3.
+  'feed.contributor.email',
+  'feed.generator',
+  'feed.icon',
+  'feed.id',
+  'feed.link',
+  'feed.logo',
+  'feed.rights',
+  'feed.subtitle',
+  'feed.tagline', // Atom 0.3.
+  'feed.title',
+  'feed.updated',
+  'feed.modified', // Atom 0.3.
+  'feed.entry.author.name',
+  'feed.entry.author.uri',
+  'feed.entry.author.url', // Atom 0.3.
+  'feed.entry.author.email',
+  'feed.entry.category',
+  'feed.entry.content',
+  'feed.entry.contributor.name',
+  'feed.entry.contributor.uri',
+  'feed.entry.contributor.url', // Atom 0.3.
+  'feed.entry.contributor.email',
+  'feed.entry.id',
+  'feed.entry.link',
+  'feed.entry.published',
+  'feed.entry.issued', // Atom 0.3.
+  'feed.entry.created', // Atom 0.3.
+  'feed.entry.rights',
+  'feed.entry.source.author.name',
+  'feed.entry.source.author.uri',
+  'feed.entry.source.author.url', // Atom 0.3.
+  'feed.entry.source.author.email',
+  'feed.entry.source.category',
+  'feed.entry.source.contributor.name',
+  'feed.entry.source.contributor.uri',
+  'feed.entry.source.contributor.url', // Atom 0.3.
+  'feed.entry.source.contributor.email',
+  'feed.entry.source.generator',
+  'feed.entry.source.icon',
+  'feed.entry.source.id',
+  'feed.entry.source.link',
+  'feed.entry.source.logo',
+  'feed.entry.source.rights',
+  'feed.entry.source.subtitle',
+  'feed.entry.source.title,',
+  'feed.entry.source.updated',
+  'feed.entry.source.modified', // Atom 0.3.
+  'feed.entry.summary',
+  'feed.entry.title',
+  'feed.entry.updated',
+  'feed.entry.modified', // Atom 0.3.
+]
 
 export const parser = new XMLParser({
   alwaysCreateTextNode: true,
