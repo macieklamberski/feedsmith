d8aa142c638e78c9b6250a05a8a2e0951b763c18
Maciej Lamberski <maciej@lamberski.com>
2025-03-19 19:57:37 +0100

    Add RDF feed type to a general Feed type

--- common/types.ts
+++ common/types.ts
@@ -1,6 +1,7 @@
-import type { Feed as JsonFeed } from '../feeds/json/types'
-import type { Feed as RssFeed } from '../feeds/rss/types'
 import type { Feed as AtomFeed } from '../feeds/atom/types'
+import type { Feed as JsonFeed } from '../feeds/json/types'
+import type { Feed as RdfFeed } from '../feeds/rdf/types'
+import type { Feed as RssFeed } from '../feeds/rss/types'
 
 export type ParseLevel = 'strict' | 'skip' | 'coerce'
 
@@ -11,7 +12,7 @@ export type Unreliable = any // Was: { [key: string]: Unreliable } | undefined.
 
 export type ParseFunction<R> = (value: Unreliable, level: NonStrictParseLevel) => R | undefined
 
-export type ParsedFeed = {
-  type: 'json' | 'rss' | 'atom'
-  feed: JsonFeed | RssFeed | AtomFeed
+export type Feed = {
+  type: 'json' | 'rss' | 'atom' | 'rdf'
+  feed: JsonFeed | RssFeed | AtomFeed | RdfFeed
 }
