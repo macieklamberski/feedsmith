ec4cbc6a24af8410d29da972ee8ac0c53418117c
Maciej Lamberski <maciej@lamberski.com>
2025-03-28 15:57:56 +0100

    feat: Create initial set of the podcast namespace parsing functions

---
+++ src/namespaces/podcast/types.ts
@@ -0,0 +1,202 @@
+export type Transcript = {
+  url: string
+  type: string
+  language?: string
+  rel?: string
+}
+
+export type Locked = {
+  value: string // Spec says: 'yes' | 'no'.
+  owner?: string
+}
+
+export type Funding = {
+  url: string
+  display?: string
+}
+
+export type Chapters = {
+  url: string
+  type: string
+}
+
+export type Soundbite = {
+  startTime: number
+  duration: number
+  display?: string
+}
+
+export type Person = {
+  display: string // Taken from #text.
+  role?: string
+  group?: string
+  img?: string
+  href?: string
+}
+
+export type Location = {
+  display: string // Taken from #text.
+  geo?: string
+  osm?: string
+}
+
+export type Season = {
+  number: number // Taken from #text.
+  name?: string
+}
+
+export type Episode = {
+  number: number
+  display?: string
+}
+
+export type Trailer = {
+  display?: string // Taken from #text.
+  url: string
+  pubdate: string // RFC2822 date.
+  length?: number
+  type?: string
+  season?: number
+}
+
+export type License = {
+  display: string
+  url?: string
+}
+
+export type AlternateEnclosure = {
+  type: string
+  length?: number
+  bitrate?: number
+  height?: number
+  lang?: string
+  title?: string
+  rel?: string
+  codecs?: string
+  default?: boolean
+  sources?: Array<Source>
+  integrity?: Integrity
+}
+
+export type Source = {
+  uri: string
+  contentType?: string
+}
+
+export type Integrity = {
+  type: string
+  value: string
+}
+
+export type Value = {
+  type: string
+  method: string
+  suggested: number
+  valueRecipients?: Array<ValueRecipient>
+  valueTimeSplits?: Array<ValueTimeSplit>
+}
+
+export type ValueRecipient = {
+  name?: string
+  customKey?: string
+  customValue?: string
+  type: string
+  address: string
+  split?: number
+  fee?: boolean
+}
+
+export type LiveItem = Item & {
+  status: string // Spec says: 'pending' | 'live' | 'ended'.
+  start: string // Date in ISO8601.
+  end?: string // Date in ISO8601.
+  contentlinks?: Array<ContentLink>
+}
+
+export type ContentLink = {
+  href: string
+  display: string
+}
+
+export type SocialInteract = {
+  uri: string
+  protocol: string
+  accountId?: string
+  accountUrl?: string
+  priority?: number
+}
+
+export type Block = {
+  value: string // Spec says: 'yes' | 'no'.
+  id?: string
+}
+
+export type Txt = {
+  display: string
+  purpose?: string
+}
+
+export type RemoteItem = {
+  feedGuid: string
+  feedUrl?: string
+  itemGuid?: string
+  medium?: string
+}
+
+export type Podroll = {
+  remoteItems?: Array<RemoteItem>
+}
+
+export type UpdateFrequency = {
+  display?: string
+  complete?: boolean
+  dtstart?: string // Date ISO8601.
+  rrule?: string
+}
+
+export type Podping = {
+  usesPodping: boolean
+}
+
+export type ValueTimeSplit = {
+  startTime: number
+  duration: number
+  remoteStartTime?: number
+  remotePercentage?: number
+  remoteItem?: RemoteItem
+  valueRecipients?: Array<ValueRecipient>
+}
+
+export type Item = {
+  transcripts?: Array<Transcript>
+  chapters?: Chapters
+  soundbites?: Array<Soundbite>
+  persons?: Array<Person>
+  location?: Location
+  season?: Season
+  episode?: Episode
+  license?: License
+  alternateEnclosures?: Array<AlternateEnclosure>
+  value?: Value
+  socialInteracts?: Array<SocialInteract>
+  txts?: Array<Txt>
+}
+
+export type Channel = {
+  locked?: Locked
+  fundings?: Array<Funding>
+  persons?: Array<Person>
+  location?: Location
+  trailers?: Array<Trailer>
+  license?: License
+  guid?: string
+  value?: Value
+  medium?: string
+  images?: string // TODO: Convert it to an array of { url, viewport }?
+  liveItems?: Array<LiveItem>
+  blocks?: Array<Block>
+  txts?: Array<Txt>
+  remoteItems?: Array<RemoteItem>
+  updateFrequency?: UpdateFrequency
+  podping?: Podping
+}
---
+++ src/namespaces/podcast/utils.ts
@@ -0,0 +1,519 @@
+import type { ParseFunction } from '../../common/types'
+import {
+  hasAllProps,
+  isNonEmptyObject,
+  isObject,
+  omitUndefinedFromObject,
+  parseArrayOf,
+  parseBoolean,
+  parseNumber,
+  parseString,
+} from '../../common/utils'
+import type {
+  AlternateEnclosure,
+  Block,
+  Channel,
+  Chapters,
+  ContentLink,
+  Episode,
+  Funding,
+  Integrity,
+  Item,
+  License,
+  LiveItem,
+  Location,
+  Locked,
+  Person,
+  Podping,
+  Podroll,
+  RemoteItem,
+  Season,
+  SocialInteract,
+  Soundbite,
+  Source,
+  Trailer,
+  Transcript,
+  Txt,
+  UpdateFrequency,
+  Value,
+  ValueRecipient,
+  ValueTimeSplit,
+} from './types'
+
+export const parseTranscript: ParseFunction<Transcript> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const transcript = omitUndefinedFromObject({
+    url: parseString(value['@url']),
+    type: parseString(value['@type']),
+    language: parseString(value['@language']),
+    rel: parseString(value['@rel']),
+  })
+
+  if (hasAllProps(transcript, ['url', 'type'])) {
+    return transcript
+  }
+}
+
+export const parseLocked: ParseFunction<Locked> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const locked = omitUndefinedFromObject({
+    value: parseString(value['@value']),
+    owner: parseString(value['@owner']),
+  })
+
+  if (hasAllProps(locked, ['value'])) {
+    return locked
+  }
+}
+
+export const parseFunding: ParseFunction<Funding> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const funding = omitUndefinedFromObject({
+    url: parseString(value['@url']),
+    display: parseString(value['#text']),
+  })
+
+  if (hasAllProps(funding, ['url'])) {
+    return funding
+  }
+}
+
+export const parseChapters: ParseFunction<Chapters> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const chapters = omitUndefinedFromObject({
+    url: parseString(value['@url']),
+    type: parseString(value['@type']),
+  })
+
+  if (hasAllProps(chapters, ['url', 'type'])) {
+    return chapters
+  }
+}
+
+export const parseSoundbite: ParseFunction<Soundbite> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const soundbite = omitUndefinedFromObject({
+    startTime: parseNumber(value['@startTime']),
+    duration: parseNumber(value['@duration']),
+    display: parseString(value['#text']),
+  })
+
+  if (hasAllProps(soundbite, ['startTime', 'duration'])) {
+    return soundbite
+  }
+}
+
+export const parsePerson: ParseFunction<Person> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const person = omitUndefinedFromObject({
+    display: parseString(value['#text']),
+    role: parseString(value['@role']),
+    group: parseString(value['@group']),
+    img: parseString(value['@img']),
+    href: parseString(value['@href']),
+  })
+
+  if (hasAllProps(person, ['display'])) {
+    return person
+  }
+}
+
+export const parseLocation: ParseFunction<Location> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const location = omitUndefinedFromObject({
+    display: parseString(value['#text']),
+    geo: parseString(value['@geo']),
+    osm: parseString(value['@osm']),
+  })
+
+  if (hasAllProps(location, ['display'])) {
+    return location
+  }
+}
+
+export const parseSeason: ParseFunction<Season> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const season = omitUndefinedFromObject({
+    number: parseNumber(value['#text']),
+    name: parseString(value['@name']),
+  })
+
+  if (hasAllProps(season, ['number'])) {
+    return season
+  }
+}
+
+export const parseEpisode: ParseFunction<Episode> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const episode = omitUndefinedFromObject({
+    number: parseNumber(value['@number']),
+    display: parseString(value['#text']),
+  })
+
+  if (hasAllProps(episode, ['number'])) {
+    return episode
+  }
+}
+
+export const parseTrailer: ParseFunction<Trailer> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const trailer = omitUndefinedFromObject({
+    display: parseString(value['#text']),
+    url: parseString(value['@url']),
+    pubdate: parseString(value['@pubdate']),
+    length: parseNumber(value['@length']),
+    type: parseString(value['@type']),
+    season: parseNumber(value['@season']),
+  })
+
+  if (hasAllProps(trailer, ['url', 'pubdate'])) {
+    return trailer
+  }
+}
+
+export const parseLicense: ParseFunction<License> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const license = omitUndefinedFromObject({
+    display: parseString(value['#text']),
+    url: parseString(value['@url']),
+  })
+
+  if (hasAllProps(license, ['display'])) {
+    return license
+  }
+}
+
+export const parseAlternateEnclosure: ParseFunction<AlternateEnclosure> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const alternateEnclosure = omitUndefinedFromObject({
+    type: parseString(value['@type']),
+    length: parseNumber(value['@length']),
+    bitrate: parseNumber(value['@bitrate']),
+    height: parseNumber(value['@height']),
+    lang: parseString(value['@lang']),
+    title: parseString(value['@title']),
+    rel: parseString(value['@rel']),
+    codecs: parseString(value['@codecs']),
+    default: parseBoolean(value['@default']),
+    sources: parseArrayOf(value['podcast:source'], parseSource),
+    integrity: parseIntegrity(value['podcast:integrity']),
+  })
+
+  if (hasAllProps(alternateEnclosure, ['type'])) {
+    return alternateEnclosure
+  }
+}
+
+export const parseSource: ParseFunction<Source> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const source = omitUndefinedFromObject({
+    uri: parseString(value['@uri']),
+    contentType: parseString(value['@contentType']),
+  })
+
+  if (hasAllProps(source, ['uri'])) {
+    return source
+  }
+}
+
+export const parseIntegrity: ParseFunction<Integrity> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const integrity = omitUndefinedFromObject({
+    type: parseString(value['@type']),
+    value: parseString(value['@value']),
+  })
+
+  if (hasAllProps(integrity, ['type', 'value'])) {
+    return integrity
+  }
+}
+
+export const parseValue: ParseFunction<Value> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const valueData = omitUndefinedFromObject({
+    type: parseString(value['@type']),
+    method: parseString(value['@method']),
+    suggested: parseNumber(value['@suggested']),
+    valueRecipients: parseArrayOf(value['podcast:valueRecipient'], parseValueRecipient),
+    valueTimeSplits: parseArrayOf(value['podcast:valueTimeSplit'], parseValueTimeSplit),
+  })
+
+  if (hasAllProps(valueData, ['type', 'method', 'suggested'])) {
+    return valueData
+  }
+}
+
+export const parseValueRecipient: ParseFunction<ValueRecipient> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const valueRecipient = omitUndefinedFromObject({
+    name: parseString(value['@name']),
+    customKey: parseString(value['@customKey']),
+    customValue: parseString(value['@customValue']),
+    type: parseString(value['@type']),
+    address: parseString(value['@address']),
+    split: parseNumber(value['@split']),
+    fee: parseBoolean(value['@fee']),
+  })
+
+  if (hasAllProps(valueRecipient, ['type', 'address'])) {
+    return valueRecipient
+  }
+}
+
+export const parseLiveItem: ParseFunction<LiveItem> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const item = parseItem(value)
+
+  if (!item) {
+    return
+  }
+
+  const liveItem = {
+    ...item,
+    status: parseString(value['@status']),
+    start: parseString(value['@start']),
+    end: parseString(value['@end']),
+    contentlinks: parseArrayOf(value['podcast:contentLink'], parseContentLink),
+  }
+
+  if (hasAllProps(liveItem, ['status', 'start'])) {
+    return liveItem
+  }
+}
+
+export const parseContentLink: ParseFunction<ContentLink> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const contentLink = omitUndefinedFromObject({
+    href: parseString(value['@href']),
+    display: parseString(value['#text']),
+  })
+
+  if (hasAllProps(contentLink, ['href', 'display'])) {
+    return contentLink
+  }
+}
+
+export const parseSocialInteract: ParseFunction<SocialInteract> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const socialInteract = omitUndefinedFromObject({
+    uri: parseString(value['@uri']),
+    protocol: parseString(value['@protocol']),
+    accountId: parseString(value['@accountId']),
+    accountUrl: parseString(value['@accountUrl']),
+    priority: parseNumber(value['@priority']),
+  })
+
+  if (hasAllProps(socialInteract, ['uri', 'protocol'])) {
+    return socialInteract
+  }
+}
+
+export const parseBlock: ParseFunction<Block> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const block = omitUndefinedFromObject({
+    value: parseString(value['@value']),
+    id: parseString(value['@id']),
+  })
+
+  if (hasAllProps(block, ['value'])) {
+    return block
+  }
+}
+
+export const parseTxt: ParseFunction<Txt> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const txt = omitUndefinedFromObject({
+    display: parseString(value['#text']),
+    purpose: parseString(value['@purpose']),
+  })
+
+  if (hasAllProps(txt, ['display'])) {
+    return txt
+  }
+}
+
+export const parseRemoteItem: ParseFunction<RemoteItem> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const remoteItem = omitUndefinedFromObject({
+    feedGuid: parseString(value['@feedGuid']),
+    feedUrl: parseString(value['@feedUrl']),
+    itemGuid: parseString(value['@itemGuid']),
+    medium: parseString(value['@medium']),
+  })
+
+  if (hasAllProps(remoteItem, ['feedGuid'])) {
+    return remoteItem
+  }
+}
+
+export const parsePodroll: ParseFunction<Podroll> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  return omitUndefinedFromObject({
+    remoteItems: parseArrayOf(value['podcast:remoteItem'], parseRemoteItem),
+  })
+}
+
+export const parseUpdateFrequency: ParseFunction<UpdateFrequency> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  return omitUndefinedFromObject({
+    display: parseString(value['#text']),
+    complete: parseBoolean(value['@complete']),
+    dtstart: parseString(value['@dtstart']),
+    rrule: parseString(value['@rrule']),
+  })
+}
+
+export const parsePodping: ParseFunction<Podping> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  return {
+    usesPodping: true,
+  }
+}
+
+export const parseValueTimeSplit: ParseFunction<ValueTimeSplit> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const valueTimeSplit = omitUndefinedFromObject({
+    startTime: parseNumber(value['@startTime']),
+    duration: parseNumber(value['@duration']),
+    remoteStartTime: parseNumber(value['@remoteStartTime']),
+    remotePercentage: parseNumber(value['@remotePercentage']),
+    remoteItem: parseRemoteItem(value['podcast:remoteItem']),
+    valueRecipients: parseArrayOf(value['podcast:valueRecipient'], parseValueRecipient),
+  })
+
+  if (hasAllProps(valueTimeSplit, ['startTime', 'duration'])) {
+    return valueTimeSplit
+  }
+}
+
+export const parseItem: ParseFunction<Item> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const item = omitUndefinedFromObject({
+    transcripts: parseArrayOf(value['podcast:transcript'], parseTranscript),
+    chapters: parseChapters(value['podcast:chapters']),
+    soundbites: parseArrayOf(value['podcast:soundbite'], parseSoundbite),
+    persons: parseArrayOf(value['podcast:person'], parsePerson),
+    location: parseLocation(value['podcast:location']),
+    season: parseSeason(value['podcast:season']),
+    episode: parseEpisode(value['podcast:episode']),
+    license: parseLicense(value['podcast:license']),
+    alternateEnclosures: parseArrayOf(value['podcast:alternateEnclosure'], parseAlternateEnclosure),
+    value: parseValue(value['podcast:value']),
+    socialInteracts: parseArrayOf(value['podcast:socialInteract'], parseSocialInteract),
+    txts: parseArrayOf(value['podcast:txt'], parseTxt),
+  })
+
+  if (isNonEmptyObject(item)) {
+    return item
+  }
+}
+
+export const parseChannel: ParseFunction<Channel> = (value) => {
+  if (!isObject(value)) {
+    return
+  }
+
+  const channel = omitUndefinedFromObject({
+    locked: parseLocked(value['podcast:locked']),
+    fundings: parseArrayOf(value['podcast:funding'], parseFunding),
+    persons: parseArrayOf(value['podcast:person'], parsePerson),
+    location: parseLocation(value['podcast:location']),
+    trailers: parseArrayOf(value['podcast:trailer'], parseTrailer),
+    license: parseLicense(value['podcast:license']),
+    guid: parseString(value['podcast:guid']?.['#text']),
+    value: parseValue(value['podcast:value']),
+    medium: parseString(value['podcast:medium']?.['#text']),
+    images: parseString(value['podcast:images']?.['#text']),
+    liveItems: parseArrayOf(value['podcast:liveItem'], parseLiveItem),
+    blocks: parseArrayOf(value['podcast:block'], parseBlock),
+    txts: parseArrayOf(value['podcast:txt'], parseTxt),
+    remoteItems: parseArrayOf(value['podcast:remoteItem'], parseRemoteItem),
+    updateFrequency: parseUpdateFrequency(value['podcast:updateFrequency']),
+    podping: value['podcast:podping'] ? parsePodping(value['podcast:podping']) : undefined,
+  })
+
+  if (isNonEmptyObject(channel)) {
+    return channel
+  }
+}
