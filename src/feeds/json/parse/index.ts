94540d02321855bcf8eb68ae48fc46608649ed6b
Maciej Lamberski <maciej@lamberski.com>
2025-03-28 16:22:18 +0100

    perf: Optimize parsing feeds by first detecting the feed type

--- src/feeds/json/parse/index.ts
+++ src/feeds/json/parse/index.ts
@@ -1,8 +1,13 @@
 import { locales } from '../../../common/config'
+import { detectJsonFeed } from '../../../index'
 import type { Feed } from './types'
 import { parseFeed } from './utils'
 
 export const parse = (value: unknown): Feed => {
+  if (!detectJsonFeed(value)) {
+    throw new Error(locales.invalid)
+  }
+
   const parsed = parseFeed(value)
 
   if (!parsed) {
