1182a4d2bf3f40f7cd58b9ef2e1ada58b3e6c052
Maciej Lamberski <maciej@lamberski.com>
2025-05-04 18:54:21 +0200

    chore: Remove unused second parameter of OPML parse function

--- src/opml/parse/index.ts
+++ src/opml/parse/index.ts
@@ -2,12 +2,7 @@ import { parser } from './config.js'
 import type { Opml } from './types.js'
 import { parseOpml } from './utils.js'
 
-export type Options = {
-  downloadExternalOutlines?: boolean
-  extraOutlineAttributes?: Array<string>
-}
-
-export const parse = (value: string, options?: Options): Opml => {
+export const parse = (value: string): Opml => {
   const object = parser.parse(value)
   const parsed = parseOpml(object)
 
