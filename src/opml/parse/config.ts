dea34a281ce767685557e9e002d79e5eb19da738
Maciej Lamberski <maciej@lamberski.com>
2025-05-04 18:05:20 +0200

    Configure stopNodes when parsing OPML files

--- src/opml/parse/config.ts
+++ src/opml/parse/config.ts
@@ -2,7 +2,20 @@ import { XMLParser } from 'fast-xml-parser'
 import { parserConfig } from '../../common/config.js'
 
 export const stopNodes = [
-  // TODO: Fill it with correct nodes.
+  'opml.head.title',
+  'opml.head.dateCreated',
+  'opml.head.dateModified',
+  'opml.head.ownerName',
+  'opml.head.ownerEmail',
+  'opml.head.ownerId',
+  'opml.head.docs',
+  'opml.head.expansionState',
+  'opml.head.vertScrollState',
+  'opml.head.windowTop',
+  'opml.head.windowLeft',
+  'opml.head.windowBottom',
+  'opml.head.windowRight',
+  '*.outline.outline',
 ]
 
 export const parser = new XMLParser({
