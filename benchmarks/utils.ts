8bc1314b6febf40c0c9ad298ed049d7342c7a674
Maciej Lamberski <maciej@lamberski.com>
2025-06-16 19:47:10 +0200

    chore: Reorder imports

--- benchmarks/utils.ts
+++ benchmarks/utils.ts
@@ -1,4 +1,4 @@
-import { readFileSync, readdirSync } from 'node:fs'
+import { readdirSync, readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import Benchmark from 'benchmark'
 import { Bench, nToMs } from 'tinybench'
