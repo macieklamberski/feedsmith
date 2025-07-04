b9eb89c6d6185ff50e5928b7f7ca68f932fdb2d1
Maciej Lamberski <maciej@lamberski.com>
2025-07-04 12:36:02 +0200

    chore: Revert the config from main

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,27 +1,5 @@
 import type { X2jOptions, XmlBuilderOptions } from 'fast-xml-parser'
 
-export const locales = {
-  unrecognized: 'Unrecognized feed format',
-  invalid: 'Invalid feed format',
-}
-
-export const namespaceUrls = {
-  atom: 'http://www.w3.org/2005/Atom',
-  content: 'http://purl.org/rss/1.0/modules/content/',
-  dc: 'http://purl.org/dc/elements/1.1/',
-  dcterms: 'http://purl.org/dc/terms/',
-  georss: 'http://www.georss.org/georss/',
-  itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd',
-  media: 'http://search.yahoo.com/mrss/',
-  podcast: 'https://podcastindex.org/namespace/1.0',
-  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-  slash: 'http://purl.org/rss/1.0/modules/slash/',
-  sy: 'http://purl.org/rss/1.0/modules/syndication/',
-  thr: 'http://purl.org/syndication/thread/1.0',
-  wfw: 'http://wellformedweb.org/CommentAPI/',
-  yt: 'http://www.youtube.com/xml/schemas/2015',
-}
-
 export const parserConfig: X2jOptions = {
   trimValues: false,
   processEntities: false,
@@ -45,4 +23,26 @@ export const builderConfig: XmlBuilderOptions = {
   attributeNamePrefix: '@',
   format: true,
   cdataPropName: '#cdata',
+}
+
+export const locales = {
+  unrecognized: 'Unrecognized feed format',
+  invalid: 'Invalid feed format',
+}
+
+export const namespaceUrls = {
+  atom: 'http://www.w3.org/2005/Atom',
+  content: 'http://purl.org/rss/1.0/modules/content/',
+  dc: 'http://purl.org/dc/elements/1.1/',
+  dcterms: 'http://purl.org/dc/terms/',
+  georss: 'http://www.georss.org/georss/',
+  itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+  media: 'http://search.yahoo.com/mrss/',
+  podcast: 'https://podcastindex.org/namespace/1.0',
+  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+  slash: 'http://purl.org/rss/1.0/modules/slash/',
+  sy: 'http://purl.org/rss/1.0/modules/syndication/',
+  thr: 'http://purl.org/syndication/thread/1.0',
+  wfw: 'http://wellformedweb.org/CommentAPI/',
+  yt: 'http://www.youtube.com/xml/schemas/2015',
 }
