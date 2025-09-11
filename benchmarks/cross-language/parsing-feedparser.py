aef474ad6656eecc0adcfca382d97642b74dd4a2
Maciej Lamberski <maciej@lamberski.com>
2025-09-11 15:43:56 +0200

    chore: Update JS benchmark package versions and file formatting

--- benchmarks/cross-language/parsing-feedparser.py
+++ benchmarks/cross-language/parsing-feedparser.py
@@ -6,7 +6,7 @@ import feedparser
 def main():
   dir_path, feed_type = sys.argv[1], sys.argv[2]
   file_pattern = os.path.join(dir_path, f"*.{feed_type}")
-  
+
   for file_path in glob.glob(file_pattern):
     if os.path.isfile(file_path):
       with open(file_path, 'r', encoding='utf-8') as f:
