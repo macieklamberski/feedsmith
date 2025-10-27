05d868f14c64e64790d4593e415fdabaf431737e
Maciej Lamberski <maciej@lamberski.com>
2025-10-27 12:19:38 +0100

    feat: Add plural variants for DC fields and deprecate singular

--- src/namespaces/dc/generate/utils.ts
+++ src/namespaces/dc/generate/utils.ts
@@ -3,6 +3,8 @@ import {
   generateCdataString,
   generateRfc3339Date,
   isObject,
+  isPresent,
+  trimArray,
   trimObject,
 } from '../../../common/utils.js'
 import type { DcNs } from '../common/types.js'
@@ -12,22 +14,49 @@ export const generateItemOrFeed: GenerateUtil<DcNs.ItemOrFeed<DateLike>> = (item
     return
   }
 
+  // TODO: Remove this once deprecated singular fields are removed in next major version.
+  const generateArray = <V>(
+    pluralValues: Array<V> | undefined,
+    singularValue: V | undefined,
+    generator: (value: V) => unknown,
+  ) => {
+    if (isPresent(pluralValues)) {
+      return trimArray(pluralValues.map(generator))
+    }
+
+    if (isPresent(singularValue)) {
+      return generator(singularValue)
+    }
+  }
+
   const value = {
-    'dc:title': generateCdataString(itemOrFeed.title),
-    'dc:creator': generateCdataString(itemOrFeed.creator),
-    'dc:subject': generateCdataString(itemOrFeed.subject),
-    'dc:description': generateCdataString(itemOrFeed.description),
-    'dc:publisher': generateCdataString(itemOrFeed.publisher),
-    'dc:contributor': generateCdataString(itemOrFeed.contributor),
-    'dc:date': generateRfc3339Date(itemOrFeed.date),
-    'dc:type': generateCdataString(itemOrFeed.type),
-    'dc:format': generateCdataString(itemOrFeed.format),
-    'dc:identifier': generateCdataString(itemOrFeed.identifier),
-    'dc:source': generateCdataString(itemOrFeed.source),
-    'dc:language': generateCdataString(itemOrFeed.language),
-    'dc:relation': generateCdataString(itemOrFeed.relation),
-    'dc:coverage': generateCdataString(itemOrFeed.coverage),
-    'dc:rights': generateCdataString(itemOrFeed.rights),
+    'dc:title': generateArray(itemOrFeed.titles, itemOrFeed.title, generateCdataString),
+    'dc:creator': generateArray(itemOrFeed.creators, itemOrFeed.creator, generateCdataString),
+    'dc:subject': generateArray(itemOrFeed.subjects, itemOrFeed.subject, generateCdataString),
+    'dc:description': generateArray(
+      itemOrFeed.descriptions,
+      itemOrFeed.description,
+      generateCdataString,
+    ),
+    'dc:publisher': generateArray(itemOrFeed.publishers, itemOrFeed.publisher, generateCdataString),
+    'dc:contributor': generateArray(
+      itemOrFeed.contributors,
+      itemOrFeed.contributor,
+      generateCdataString,
+    ),
+    'dc:date': generateArray(itemOrFeed.dates, itemOrFeed.date, generateRfc3339Date),
+    'dc:type': generateArray(itemOrFeed.types, itemOrFeed.type, generateCdataString),
+    'dc:format': generateArray(itemOrFeed.formats, itemOrFeed.format, generateCdataString),
+    'dc:identifier': generateArray(
+      itemOrFeed.identifiers,
+      itemOrFeed.identifier,
+      generateCdataString,
+    ),
+    'dc:source': generateArray(itemOrFeed.sources, itemOrFeed.source, generateCdataString),
+    'dc:language': generateArray(itemOrFeed.languages, itemOrFeed.language, generateCdataString),
+    'dc:relation': generateArray(itemOrFeed.relations, itemOrFeed.relation, generateCdataString),
+    'dc:coverage': generateArray(itemOrFeed.coverages, itemOrFeed.coverage, generateCdataString),
+    'dc:rights': generateArray(itemOrFeed.rights, undefined, generateCdataString),
   }
 
   return trimObject(value)
