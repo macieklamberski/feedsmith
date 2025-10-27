2a5f0ec6001a57746ff3ce705466cc3e3c17e8d9
Maciej Lamberski <maciej@lamberski.com>
2025-10-27 15:48:50 +0100

    chore: Update order of deprecated fields in DC, DC Terms

--- src/namespaces/dc/parse/utils.ts
+++ src/namespaces/dc/parse/utils.ts
@@ -16,7 +16,27 @@ export const retrieveItemOrFeed: ParsePartialUtil<DcNs.ItemOrFeed<string>> = (va
   }
 
   const itemOrFeed = {
-    // Singular fields (deprecated - kept for backward compatibility, returns first value)
+    titles: parseArrayOf(value['dc:title'], (value) => parseString(retrieveText(value))),
+    creators: parseArrayOf(value['dc:creator'], (value) => parseString(retrieveText(value))),
+    subjects: parseArrayOf(value['dc:subject'], (value) => parseString(retrieveText(value))),
+    descriptions: parseArrayOf(value['dc:description'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    publishers: parseArrayOf(value['dc:publisher'], (value) => parseString(retrieveText(value))),
+    contributors: parseArrayOf(value['dc:contributor'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    dates: parseArrayOf(value['dc:date'], (value) => parseDate(retrieveText(value))),
+    types: parseArrayOf(value['dc:type'], (value) => parseString(retrieveText(value))),
+    formats: parseArrayOf(value['dc:format'], (value) => parseString(retrieveText(value))),
+    identifiers: parseArrayOf(value['dc:identifier'], (value) => parseString(retrieveText(value))),
+    sources: parseArrayOf(value['dc:source'], (value) => parseString(retrieveText(value))),
+    languages: parseArrayOf(value['dc:language'], (value) => parseString(retrieveText(value))),
+    relations: parseArrayOf(value['dc:relation'], (value) => parseString(retrieveText(value))),
+    coverages: parseArrayOf(value['dc:coverage'], (value) => parseString(retrieveText(value))),
+    rights: parseArrayOf(value['dc:rights'], (value) => parseString(retrieveText(value))),
+
+    // Deprecated fields for backward compatibility.
     title: parseSingularOf(value['dc:title'], (value) => parseString(retrieveText(value))),
     creator: parseSingularOf(value['dc:creator'], (value) => parseString(retrieveText(value))),
     subject: parseSingularOf(value['dc:subject'], (value) => parseString(retrieveText(value))),
@@ -37,27 +57,6 @@ export const retrieveItemOrFeed: ParsePartialUtil<DcNs.ItemOrFeed<string>> = (va
     language: parseSingularOf(value['dc:language'], (value) => parseString(retrieveText(value))),
     relation: parseSingularOf(value['dc:relation'], (value) => parseString(retrieveText(value))),
     coverage: parseSingularOf(value['dc:coverage'], (value) => parseString(retrieveText(value))),
-
-    // Plural fields (correct - all DC elements are repeatable, returns all values)
-    titles: parseArrayOf(value['dc:title'], (value) => parseString(retrieveText(value))),
-    creators: parseArrayOf(value['dc:creator'], (value) => parseString(retrieveText(value))),
-    subjects: parseArrayOf(value['dc:subject'], (value) => parseString(retrieveText(value))),
-    descriptions: parseArrayOf(value['dc:description'], (value) =>
-      parseString(retrieveText(value)),
-    ),
-    publishers: parseArrayOf(value['dc:publisher'], (value) => parseString(retrieveText(value))),
-    contributors: parseArrayOf(value['dc:contributor'], (value) =>
-      parseString(retrieveText(value)),
-    ),
-    dates: parseArrayOf(value['dc:date'], (value) => parseDate(retrieveText(value))),
-    types: parseArrayOf(value['dc:type'], (value) => parseString(retrieveText(value))),
-    formats: parseArrayOf(value['dc:format'], (value) => parseString(retrieveText(value))),
-    identifiers: parseArrayOf(value['dc:identifier'], (value) => parseString(retrieveText(value))),
-    sources: parseArrayOf(value['dc:source'], (value) => parseString(retrieveText(value))),
-    languages: parseArrayOf(value['dc:language'], (value) => parseString(retrieveText(value))),
-    relations: parseArrayOf(value['dc:relation'], (value) => parseString(retrieveText(value))),
-    coverages: parseArrayOf(value['dc:coverage'], (value) => parseString(retrieveText(value))),
-    rights: parseArrayOf(value['dc:rights'], (value) => parseString(retrieveText(value))),
   }
 
   return trimObject(itemOrFeed)
