d807bc9a23801fbcfccebe8bca8d6101dae8a9a1
Maciej Lamberski <maciej@lamberski.com>
2025-10-27 12:19:38 +0100

    feat: Add plural variants for DC fields and deprecate singular (except rights)

--- src/feeds/rdf/parse/utils.test.ts
+++ src/feeds/rdf/parse/utils.test.ts
@@ -411,7 +411,7 @@ describe('parseItem', () => {
     const expected = {
       title: 'Example Entry',
       link: 'http://example.com',
-      dc: { creator: 'John Doe' },
+      dc: { creator: 'John Doe', creators: ['John Doe'] },
     }
 
     expect(parseItem(value)).toEqual(expected)
@@ -960,7 +960,7 @@ describe('parseFeed', () => {
           link: 'https://example.com/item1',
         },
       ],
-      dc: { creator: 'John Doe' },
+      dc: { creator: 'John Doe', creators: ['John Doe'] },
     }
 
     expect(parseFeed(value)).toEqual(expected)
