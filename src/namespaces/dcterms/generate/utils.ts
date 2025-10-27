ab504a4be845703c41be5e230c8a711480505ac1
Maciej Lamberski <maciej@lamberski.com>
2025-10-27 12:20:20 +0100

    feat: Add plural variants for DC Terms fields and deprecate singular

--- src/namespaces/dcterms/generate/utils.ts
+++ src/namespaces/dcterms/generate/utils.ts
@@ -3,6 +3,8 @@ import {
   generateCdataString,
   generateRfc3339Date,
   isObject,
+  isPresent,
+  trimArray,
   trimObject,
 } from '../../../common/utils.js'
 import type { DctermsNs } from '../common/types.js'
@@ -12,62 +14,217 @@ export const generateItemOrFeed: GenerateUtil<DctermsNs.ItemOrFeed<DateLike>> = 
     return
   }
 
+  // TODO: Remove this once deprecated singular fields are removed in next major version.
+  const generateArray = <V>(
+    pluralValues: Array<V> | undefined,
+    singularValue: V | undefined,
+    generator: (value: V) => unknown,
+  ) => {
+    if (isPresent(pluralValues)) {
+      return trimArray(pluralValues.map(generator))
+    }
+
+    if (isPresent(singularValue)) {
+      return generator(singularValue)
+    }
+  }
+
   const value = {
-    'dcterms:abstract': generateCdataString(itemOrFeed.abstract),
-    'dcterms:accessRights': generateCdataString(itemOrFeed.accessRights),
-    'dcterms:accrualMethod': generateCdataString(itemOrFeed.accrualMethod),
-    'dcterms:accrualPeriodicity': generateCdataString(itemOrFeed.accrualPeriodicity),
-    'dcterms:accrualPolicy': generateCdataString(itemOrFeed.accrualPolicy),
-    'dcterms:alternative': generateCdataString(itemOrFeed.alternative),
-    'dcterms:audience': generateCdataString(itemOrFeed.audience),
-    'dcterms:available': generateRfc3339Date(itemOrFeed.available),
-    'dcterms:bibliographicCitation': generateCdataString(itemOrFeed.bibliographicCitation),
-    'dcterms:conformsTo': generateCdataString(itemOrFeed.conformsTo),
-    'dcterms:contributor': generateCdataString(itemOrFeed.contributor),
-    'dcterms:coverage': generateCdataString(itemOrFeed.coverage),
-    'dcterms:created': generateRfc3339Date(itemOrFeed.created),
-    'dcterms:creator': generateCdataString(itemOrFeed.creator),
-    'dcterms:date': generateRfc3339Date(itemOrFeed.date),
-    'dcterms:dateAccepted': generateRfc3339Date(itemOrFeed.dateAccepted),
-    'dcterms:dateCopyrighted': generateRfc3339Date(itemOrFeed.dateCopyrighted),
-    'dcterms:dateSubmitted': generateRfc3339Date(itemOrFeed.dateSubmitted),
-    'dcterms:description': generateCdataString(itemOrFeed.description),
-    'dcterms:educationLevel': generateCdataString(itemOrFeed.educationLevel),
-    'dcterms:extent': generateCdataString(itemOrFeed.extent),
-    'dcterms:format': generateCdataString(itemOrFeed.format),
-    'dcterms:hasFormat': generateCdataString(itemOrFeed.hasFormat),
-    'dcterms:hasPart': generateCdataString(itemOrFeed.hasPart),
-    'dcterms:hasVersion': generateCdataString(itemOrFeed.hasVersion),
-    'dcterms:identifier': generateCdataString(itemOrFeed.identifier),
-    'dcterms:instructionalMethod': generateCdataString(itemOrFeed.instructionalMethod),
-    'dcterms:isFormatOf': generateCdataString(itemOrFeed.isFormatOf),
-    'dcterms:isPartOf': generateCdataString(itemOrFeed.isPartOf),
-    'dcterms:isReferencedBy': generateCdataString(itemOrFeed.isReferencedBy),
-    'dcterms:isReplacedBy': generateCdataString(itemOrFeed.isReplacedBy),
-    'dcterms:isRequiredBy': generateCdataString(itemOrFeed.isRequiredBy),
-    'dcterms:issued': generateRfc3339Date(itemOrFeed.issued),
-    'dcterms:isVersionOf': generateCdataString(itemOrFeed.isVersionOf),
-    'dcterms:language': generateCdataString(itemOrFeed.language),
-    'dcterms:license': generateCdataString(itemOrFeed.license),
-    'dcterms:mediator': generateCdataString(itemOrFeed.mediator),
-    'dcterms:medium': generateCdataString(itemOrFeed.medium),
-    'dcterms:modified': generateRfc3339Date(itemOrFeed.modified),
-    'dcterms:provenance': generateCdataString(itemOrFeed.provenance),
-    'dcterms:publisher': generateCdataString(itemOrFeed.publisher),
-    'dcterms:references': generateCdataString(itemOrFeed.references),
-    'dcterms:relation': generateCdataString(itemOrFeed.relation),
-    'dcterms:replaces': generateCdataString(itemOrFeed.replaces),
-    'dcterms:requires': generateCdataString(itemOrFeed.requires),
-    'dcterms:rights': generateCdataString(itemOrFeed.rights),
-    'dcterms:rightsHolder': generateCdataString(itemOrFeed.rightsHolder),
-    'dcterms:source': generateCdataString(itemOrFeed.source),
-    'dcterms:spatial': generateCdataString(itemOrFeed.spatial),
-    'dcterms:subject': generateCdataString(itemOrFeed.subject),
-    'dcterms:tableOfContents': generateCdataString(itemOrFeed.tableOfContents),
-    'dcterms:temporal': generateCdataString(itemOrFeed.temporal),
-    'dcterms:title': generateCdataString(itemOrFeed.title),
-    'dcterms:type': generateCdataString(itemOrFeed.type),
-    'dcterms:valid': generateRfc3339Date(itemOrFeed.valid),
+    'dcterms:abstract': generateArray(
+      itemOrFeed.abstracts,
+      itemOrFeed.abstract,
+      generateCdataString,
+    ),
+    'dcterms:accessRights': generateArray(itemOrFeed.accessRights, undefined, generateCdataString),
+    'dcterms:accrualMethod': generateArray(
+      itemOrFeed.accrualMethods,
+      itemOrFeed.accrualMethod,
+      generateCdataString,
+    ),
+    'dcterms:accrualPeriodicity': generateArray(
+      itemOrFeed.accrualPeriodicities,
+      itemOrFeed.accrualPeriodicity,
+      generateCdataString,
+    ),
+    'dcterms:accrualPolicy': generateArray(
+      itemOrFeed.accrualPolicies,
+      itemOrFeed.accrualPolicy,
+      generateCdataString,
+    ),
+    'dcterms:alternative': generateArray(
+      itemOrFeed.alternatives,
+      itemOrFeed.alternative,
+      generateCdataString,
+    ),
+    'dcterms:audience': generateArray(
+      itemOrFeed.audiences,
+      itemOrFeed.audience,
+      generateCdataString,
+    ),
+    'dcterms:available': generateArray(
+      itemOrFeed.availables,
+      itemOrFeed.available,
+      generateRfc3339Date,
+    ),
+    'dcterms:bibliographicCitation': generateArray(
+      itemOrFeed.bibliographicCitations,
+      itemOrFeed.bibliographicCitation,
+      generateCdataString,
+    ),
+    'dcterms:conformsTo': generateArray(
+      itemOrFeed.conformsTos,
+      itemOrFeed.conformsTo,
+      generateCdataString,
+    ),
+    'dcterms:contributor': generateArray(
+      itemOrFeed.contributors,
+      itemOrFeed.contributor,
+      generateCdataString,
+    ),
+    'dcterms:coverage': generateArray(
+      itemOrFeed.coverages,
+      itemOrFeed.coverage,
+      generateCdataString,
+    ),
+    'dcterms:created': generateArray(itemOrFeed.createds, itemOrFeed.created, generateRfc3339Date),
+    'dcterms:creator': generateArray(itemOrFeed.creators, itemOrFeed.creator, generateCdataString),
+    'dcterms:date': generateArray(itemOrFeed.dates, itemOrFeed.date, generateRfc3339Date),
+    'dcterms:dateAccepted': generateArray(
+      itemOrFeed.dateAccepteds,
+      itemOrFeed.dateAccepted,
+      generateRfc3339Date,
+    ),
+    'dcterms:dateCopyrighted': generateArray(
+      itemOrFeed.dateCopyrighteds,
+      itemOrFeed.dateCopyrighted,
+      generateRfc3339Date,
+    ),
+    'dcterms:dateSubmitted': generateArray(
+      itemOrFeed.dateSubmitteds,
+      itemOrFeed.dateSubmitted,
+      generateRfc3339Date,
+    ),
+    'dcterms:description': generateArray(
+      itemOrFeed.descriptions,
+      itemOrFeed.description,
+      generateCdataString,
+    ),
+    'dcterms:educationLevel': generateArray(
+      itemOrFeed.educationLevels,
+      itemOrFeed.educationLevel,
+      generateCdataString,
+    ),
+    'dcterms:extent': generateArray(itemOrFeed.extents, itemOrFeed.extent, generateCdataString),
+    'dcterms:format': generateArray(itemOrFeed.formats, itemOrFeed.format, generateCdataString),
+    'dcterms:hasFormat': generateArray(
+      itemOrFeed.hasFormats,
+      itemOrFeed.hasFormat,
+      generateCdataString,
+    ),
+    'dcterms:hasPart': generateArray(itemOrFeed.hasParts, itemOrFeed.hasPart, generateCdataString),
+    'dcterms:hasVersion': generateArray(
+      itemOrFeed.hasVersions,
+      itemOrFeed.hasVersion,
+      generateCdataString,
+    ),
+    'dcterms:identifier': generateArray(
+      itemOrFeed.identifiers,
+      itemOrFeed.identifier,
+      generateCdataString,
+    ),
+    'dcterms:instructionalMethod': generateArray(
+      itemOrFeed.instructionalMethods,
+      itemOrFeed.instructionalMethod,
+      generateCdataString,
+    ),
+    'dcterms:isFormatOf': generateArray(
+      itemOrFeed.isFormatOfs,
+      itemOrFeed.isFormatOf,
+      generateCdataString,
+    ),
+    'dcterms:isPartOf': generateArray(
+      itemOrFeed.isPartOfs,
+      itemOrFeed.isPartOf,
+      generateCdataString,
+    ),
+    'dcterms:isReferencedBy': generateArray(
+      itemOrFeed.isReferencedBys,
+      itemOrFeed.isReferencedBy,
+      generateCdataString,
+    ),
+    'dcterms:isReplacedBy': generateArray(
+      itemOrFeed.isReplacedBys,
+      itemOrFeed.isReplacedBy,
+      generateCdataString,
+    ),
+    'dcterms:isRequiredBy': generateArray(
+      itemOrFeed.isRequiredBys,
+      itemOrFeed.isRequiredBy,
+      generateCdataString,
+    ),
+    'dcterms:issued': generateArray(itemOrFeed.issueds, itemOrFeed.issued, generateRfc3339Date),
+    'dcterms:isVersionOf': generateArray(
+      itemOrFeed.isVersionOfs,
+      itemOrFeed.isVersionOf,
+      generateCdataString,
+    ),
+    'dcterms:language': generateArray(
+      itemOrFeed.languages,
+      itemOrFeed.language,
+      generateCdataString,
+    ),
+    'dcterms:license': generateArray(itemOrFeed.licenses, itemOrFeed.license, generateCdataString),
+    'dcterms:mediator': generateArray(
+      itemOrFeed.mediators,
+      itemOrFeed.mediator,
+      generateCdataString,
+    ),
+    'dcterms:medium': generateArray(itemOrFeed.mediums, itemOrFeed.medium, generateCdataString),
+    'dcterms:modified': generateArray(
+      itemOrFeed.modifieds,
+      itemOrFeed.modified,
+      generateRfc3339Date,
+    ),
+    'dcterms:provenance': generateArray(
+      itemOrFeed.provenances,
+      itemOrFeed.provenance,
+      generateCdataString,
+    ),
+    'dcterms:publisher': generateArray(
+      itemOrFeed.publishers,
+      itemOrFeed.publisher,
+      generateCdataString,
+    ),
+    'dcterms:references': generateArray(itemOrFeed.references, undefined, generateCdataString),
+    'dcterms:relation': generateArray(
+      itemOrFeed.relations,
+      itemOrFeed.relation,
+      generateCdataString,
+    ),
+    'dcterms:replaces': generateArray(itemOrFeed.replaces, undefined, generateCdataString),
+    'dcterms:requires': generateArray(itemOrFeed.requires, undefined, generateCdataString),
+    'dcterms:rights': generateArray(itemOrFeed.rights, undefined, generateCdataString),
+    'dcterms:rightsHolder': generateArray(
+      itemOrFeed.rightsHolders,
+      itemOrFeed.rightsHolder,
+      generateCdataString,
+    ),
+    'dcterms:source': generateArray(itemOrFeed.sources, itemOrFeed.source, generateCdataString),
+    'dcterms:spatial': generateArray(itemOrFeed.spatials, itemOrFeed.spatial, generateCdataString),
+    'dcterms:subject': generateArray(itemOrFeed.subjects, itemOrFeed.subject, generateCdataString),
+    'dcterms:tableOfContents': generateArray(
+      itemOrFeed.tableOfContents,
+      undefined,
+      generateCdataString,
+    ),
+    'dcterms:temporal': generateArray(
+      itemOrFeed.temporals,
+      itemOrFeed.temporal,
+      generateCdataString,
+    ),
+    'dcterms:title': generateArray(itemOrFeed.titles, itemOrFeed.title, generateCdataString),
+    'dcterms:type': generateArray(itemOrFeed.types, itemOrFeed.type, generateCdataString),
+    'dcterms:valid': generateArray(itemOrFeed.valids, itemOrFeed.valid, generateRfc3339Date),
   }
 
   return trimObject(value)
--- src/namespaces/dcterms/parse/utils.ts
+++ src/namespaces/dcterms/parse/utils.ts
@@ -1,6 +1,7 @@
 import type { ParsePartialUtil } from '../../../common/types.js'
 import {
   isObject,
+  parseArrayOf,
   parseDate,
   parseSingularOf,
   parseString,
@@ -15,10 +16,8 @@ export const retrieveItemOrFeed: ParsePartialUtil<DctermsNs.ItemOrFeed<string>> 
   }
 
   const itemOrFeed = {
+    // Singular fields (deprecated - kept for backward compatibility, returns first value)
     abstract: parseSingularOf(value['dcterms:abstract'], (value) =>
-      parseString(retrieveText(value)),
-    ),
-    accessRights: parseSingularOf(value['dcterms:accessrights'], (value) =>
       parseString(retrieveText(value)),
     ),
     accrualMethod: parseSingularOf(value['dcterms:accrualmethod'], (value) =>
@@ -118,34 +117,132 @@ export const retrieveItemOrFeed: ParsePartialUtil<DctermsNs.ItemOrFeed<string>> 
     publisher: parseSingularOf(value['dcterms:publisher'], (value) =>
       parseString(retrieveText(value)),
     ),
-    references: parseSingularOf(value['dcterms:references'], (value) =>
-      parseString(retrieveText(value)),
-    ),
     relation: parseSingularOf(value['dcterms:relation'], (value) =>
       parseString(retrieveText(value)),
     ),
-    replaces: parseSingularOf(value['dcterms:replaces'], (value) =>
-      parseString(retrieveText(value)),
-    ),
-    requires: parseSingularOf(value['dcterms:requires'], (value) =>
-      parseString(retrieveText(value)),
-    ),
-    rights: parseSingularOf(value['dcterms:rights'], (value) => parseString(retrieveText(value))),
     rightsHolder: parseSingularOf(value['dcterms:rightsholder'], (value) =>
       parseString(retrieveText(value)),
     ),
     source: parseSingularOf(value['dcterms:source'], (value) => parseString(retrieveText(value))),
     spatial: parseSingularOf(value['dcterms:spatial'], (value) => parseString(retrieveText(value))),
     subject: parseSingularOf(value['dcterms:subject'], (value) => parseString(retrieveText(value))),
-    tableOfContents: parseSingularOf(value['dcterms:tableofcontents'], (value) =>
-      parseString(retrieveText(value)),
-    ),
     temporal: parseSingularOf(value['dcterms:temporal'], (value) =>
       parseString(retrieveText(value)),
     ),
     title: parseSingularOf(value['dcterms:title'], (value) => parseString(retrieveText(value))),
     type: parseSingularOf(value['dcterms:type'], (value) => parseString(retrieveText(value))),
     valid: parseSingularOf(value['dcterms:valid'], (value) => parseDate(retrieveText(value))),
+
+    // Plural fields (correct - all DC Terms properties are repeatable, returns all values)
+    abstracts: parseArrayOf(value['dcterms:abstract'], (value) => parseString(retrieveText(value))),
+    accessRights: parseArrayOf(value['dcterms:accessrights'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    accrualMethods: parseArrayOf(value['dcterms:accrualmethod'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    accrualPeriodicities: parseArrayOf(value['dcterms:accrualperiodicity'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    accrualPolicies: parseArrayOf(value['dcterms:accrualpolicy'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    alternatives: parseArrayOf(value['dcterms:alternative'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    audiences: parseArrayOf(value['dcterms:audience'], (value) => parseString(retrieveText(value))),
+    availables: parseArrayOf(value['dcterms:available'], (value) => parseDate(retrieveText(value))),
+    bibliographicCitations: parseArrayOf(value['dcterms:bibliographiccitation'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    conformsTos: parseArrayOf(value['dcterms:conformsto'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    contributors: parseArrayOf(value['dcterms:contributor'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    coverages: parseArrayOf(value['dcterms:coverage'], (value) => parseString(retrieveText(value))),
+    createds: parseArrayOf(value['dcterms:created'], (value) => parseDate(retrieveText(value))),
+    creators: parseArrayOf(value['dcterms:creator'], (value) => parseString(retrieveText(value))),
+    dates: parseArrayOf(value['dcterms:date'], (value) => parseDate(retrieveText(value))),
+    dateAccepteds: parseArrayOf(value['dcterms:dateaccepted'], (value) =>
+      parseDate(retrieveText(value)),
+    ),
+    dateCopyrighteds: parseArrayOf(value['dcterms:datecopyrighted'], (value) =>
+      parseDate(retrieveText(value)),
+    ),
+    dateSubmitteds: parseArrayOf(value['dcterms:datesubmitted'], (value) =>
+      parseDate(retrieveText(value)),
+    ),
+    descriptions: parseArrayOf(value['dcterms:description'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    educationLevels: parseArrayOf(value['dcterms:educationlevel'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    extents: parseArrayOf(value['dcterms:extent'], (value) => parseString(retrieveText(value))),
+    formats: parseArrayOf(value['dcterms:format'], (value) => parseString(retrieveText(value))),
+    hasFormats: parseArrayOf(value['dcterms:hasformat'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    hasParts: parseArrayOf(value['dcterms:haspart'], (value) => parseString(retrieveText(value))),
+    hasVersions: parseArrayOf(value['dcterms:hasversion'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    identifiers: parseArrayOf(value['dcterms:identifier'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    instructionalMethods: parseArrayOf(value['dcterms:instructionalmethod'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    isFormatOfs: parseArrayOf(value['dcterms:isformatof'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    isPartOfs: parseArrayOf(value['dcterms:ispartof'], (value) => parseString(retrieveText(value))),
+    isReferencedBys: parseArrayOf(value['dcterms:isreferencedby'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    isReplacedBys: parseArrayOf(value['dcterms:isreplacedby'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    isRequiredBys: parseArrayOf(value['dcterms:isrequiredby'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    issueds: parseArrayOf(value['dcterms:issued'], (value) => parseDate(retrieveText(value))),
+    isVersionOfs: parseArrayOf(value['dcterms:isversionof'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    languages: parseArrayOf(value['dcterms:language'], (value) => parseString(retrieveText(value))),
+    licenses: parseArrayOf(value['dcterms:license'], (value) => parseString(retrieveText(value))),
+    mediators: parseArrayOf(value['dcterms:mediator'], (value) => parseString(retrieveText(value))),
+    mediums: parseArrayOf(value['dcterms:medium'], (value) => parseString(retrieveText(value))),
+    modifieds: parseArrayOf(value['dcterms:modified'], (value) => parseDate(retrieveText(value))),
+    provenances: parseArrayOf(value['dcterms:provenance'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    publishers: parseArrayOf(value['dcterms:publisher'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    references: parseArrayOf(value['dcterms:references'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    relations: parseArrayOf(value['dcterms:relation'], (value) => parseString(retrieveText(value))),
+    replaces: parseArrayOf(value['dcterms:replaces'], (value) => parseString(retrieveText(value))),
+    requires: parseArrayOf(value['dcterms:requires'], (value) => parseString(retrieveText(value))),
+    rights: parseArrayOf(value['dcterms:rights'], (value) => parseString(retrieveText(value))),
+    rightsHolders: parseArrayOf(value['dcterms:rightsholder'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    sources: parseArrayOf(value['dcterms:source'], (value) => parseString(retrieveText(value))),
+    spatials: parseArrayOf(value['dcterms:spatial'], (value) => parseString(retrieveText(value))),
+    subjects: parseArrayOf(value['dcterms:subject'], (value) => parseString(retrieveText(value))),
+    tableOfContents: parseArrayOf(value['dcterms:tableofcontents'], (value) =>
+      parseString(retrieveText(value)),
+    ),
+    temporals: parseArrayOf(value['dcterms:temporal'], (value) => parseString(retrieveText(value))),
+    titles: parseArrayOf(value['dcterms:title'], (value) => parseString(retrieveText(value))),
+    types: parseArrayOf(value['dcterms:type'], (value) => parseString(retrieveText(value))),
+    valids: parseArrayOf(value['dcterms:valid'], (value) => parseDate(retrieveText(value))),
   }
 
   return trimObject(itemOrFeed)
