02945376a54b97f44a42766a0f5c58e38cfef6b9
Maciej Lamberski <maciej@lamberski.com>
2025-10-28 22:26:39 +0100

    feat: Add common alternative namespace URI for Podcast

--- src/common/config.ts
+++ src/common/config.ts
@@ -94,6 +94,7 @@ export const namespaceUris = {
     'http://podcastindex.org/namespace/1.0',
     'https://podcastindex.org/namespace/1.0/',
     'http://podcastindex.org/namespace/1.0/',
+    'https://github.com/Podcastindex-org/podcast-namespace/blob/main/docs/1.0.md',
   ],
   psc: [
     'http://podlove.org/simple-chapters', // Official URI.
