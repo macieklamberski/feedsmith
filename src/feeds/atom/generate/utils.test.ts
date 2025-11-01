f61af8c047519eaf63ef2319af058526c3de6cf0
Maciej Lamberski <maciej@lamberski.com>
2025-11-01 19:02:24 +0100

    chore: Update reference files after changes in OpenSearch namespace

--- src/feeds/atom/generate/utils.test.ts
+++ src/feeds/atom/generate/utils.test.ts
@@ -1411,10 +1411,12 @@ describe('generateFeed', () => {
         totalResults: 1000,
         startIndex: 21,
         itemsPerPage: 10,
-        query: {
-          role: 'request',
-          searchTerms: 'quantum computing',
-        },
+        queries: [
+          {
+            role: 'request',
+            searchTerms: 'quantum computing',
+          },
+        ],
       },
     }
     const expected = {
