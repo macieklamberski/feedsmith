f450451d3ec41b61e2aca2f07613133e44935e24
Maciej Lamberski <maciej@lamberski.com>
2025-11-03 15:05:46 +0100

    chore: Add alternative namespace URIs for APP

--- src/common/config.ts
+++ src/common/config.ts
@@ -51,6 +51,9 @@ export const namespaceUris = {
   ],
   app: [
     'http://www.w3.org/2007/app', // Official URI.
+    'https://www.w3.org/2007/app',
+    'http://www.w3.org/2007/app/',
+    'https://www.w3.org/2007/app/',
   ],
   dc: [
     'http://purl.org/dc/elements/1.1/', // Official URI.
