7262eca97023c2492537d00c2b87d7f159f675a3
Maciek Lamberski <maciej@lamberski.com>
2026-03-15 18:38:44 +0100

    feat: Add GenerateError to JSON feed generator

--- src/feeds/json/generate/index.ts
+++ src/feeds/json/generate/index.ts
@@ -1,7 +1,15 @@
+import { locales } from '../../../common/config.js'
+import { GenerateError } from '../../../common/errors.js'
 import type { DateLike, GenerateMainJson } from '../../../common/types.js'
 import type { Json } from '../common/types.js'
 import { generateFeed } from './utils.js'
 
 export const generate: GenerateMainJson<Json.Feed<DateLike>, Json.Feed<Date, true>> = (value) => {
-  return generateFeed(value)
+  const generated = generateFeed(value)
+
+  if (!generated) {
+    throw new GenerateError(locales.invalidInputJson)
+  }
+
+  return generated
 }
