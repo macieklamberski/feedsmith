f57566029740a1177e56ecdc70452c5645d14af7
Maciek Lamberski <maciej@lamberski.com>
2026-09-21 14:46:56 +0700

    fix: Recognize the RSS 2.0 specification address as its namespace

--- src/namespaces/rss2/common/config.ts
+++ src/namespaces/rss2/common/config.ts
@@ -3,4 +3,6 @@ export const uris = [
   'https://backend.userland.com/rss2',
   'http://backend.userland.com/rss2/',
   'https://backend.userland.com/rss2/',
+  'https://cyber.harvard.edu/rss/rss.html',
+  'http://cyber.harvard.edu/rss/rss.html',
 ]
